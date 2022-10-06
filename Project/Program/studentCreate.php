b30bf6570831a87109d19f24b4409b2eefcc9d0a
szlzs05 <zsoltisw@gmail.com>
2022-10-06 08:51:08 +0200

    Add student gomb hozzáadása link és h4 tagben

---
+++ Project/Program/studentCreate.php
@@ -0,0 +1,17 @@
+<div class="container mt-5">
+        <?php
+            include 'message.php';
+        ?>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Add student <a href="index.php" class="btn btn-danger float-end">Back</a></h4>
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
