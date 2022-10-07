5bd61658cbaa7dacbb06326313b98b2e25ff29d3
szlzs05 <zsoltisw@gmail.com>
2022-10-07 10:07:21 +0200

    Kérés esetén annak vizsgálata, hogy minden mező ki lett-e töltve

--- Project/Program/code.php
+++ Project/Program/code.php
@@ -3,6 +3,8 @@
 
     if (isset($_POST['save_student']))
     {
-        
+        if (empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['course'])) {
+            
+        }
     }
 ?>
