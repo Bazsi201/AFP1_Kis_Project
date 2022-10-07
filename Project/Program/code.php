5f3b7c862fdf234425ff02c577b097faa55d7eec
szlzs05 <zsoltisw@gmail.com>
2022-10-07 10:13:30 +0200

    Igaz ág esetén a name mező tartalmának elérése

--- Project/Program/code.php
+++ Project/Program/code.php
@@ -4,7 +4,10 @@
     if (isset($_POST['save_student']))
     {
         if (empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['course'])) {
-            
+        }
+        else
+        {
+            $name = mysqli_real_escape_string($con,$_POST['name']);
         }
     }
 ?>
