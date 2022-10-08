46b16bedf158ca34a0754060ade5e2d4674a0bac
kmartin001 <kaponyamartin@gmail.com>
2022-10-08 21:26:11 +0200

    táblázat kialakítása: sorok, oszlopok, fejléc

--- Project/Program/index.php
+++ Project/Program/index.php
@@ -1,3 +1,26 @@
 <?php include('includes/header.php'); ?>
 
+<div class="container mt-5">
+
+        <?php include('message.php'); ?>
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-body">
+                            <table class="table table-bordered table-striped align-middle" style="text-align:center">
+                                <thead>
+                                    <tr>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 <?php include('includes/footer.php'); ?>
