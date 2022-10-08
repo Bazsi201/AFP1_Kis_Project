9e7d9eceeebec2352369048c30275771f156b264
kmartin001 <kaponyamartin@gmail.com>
2022-10-08 21:14:37 +0200

    üzenet bezáró gomb létrehozása

--- Project/Program/includes/message.php
+++ Project/Program/includes/message.php
@@ -4,6 +4,7 @@
 
 <div class="alert alert-warning alert-dismissible fade show" role="alert">
     <strong>Hey!</strong> <?= $_SESSION['message']; ?>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 
 <?php
