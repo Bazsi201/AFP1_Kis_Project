7603bbfeefb206cd4c81606d4c052f92f7fb0ff6
kmartin001 <kaponyamartin@gmail.com>
2022-10-08 21:13:26 +0200

    az üzenet keretének létrehozása, formázása

--- Project/Program/includes/message.php
+++ Project/Program/includes/message.php
@@ -2,6 +2,10 @@
     if (isset($_SESSION['message'])) :
 ?>
 
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+    
+</div>
+
 <?php
     unset($_SESSION['message']);
     endif;
