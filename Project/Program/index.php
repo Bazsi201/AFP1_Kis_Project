79b86d1a0690b0ce220b3f0248dfd8938c4876a6
Bazsi201 <m.bazsi201@gmail.com>
2022-10-09 13:25:50 +0200

    message.php fájl hibás elérési útjának javítása az include-ban

--- Project/Program/index.php
+++ Project/Program/index.php
@@ -7,7 +7,7 @@
 
 <div class="container mt-5">
 
-        <?php include('message.php'); ?>
+        <?php include('includes/message.php'); ?>
 
         <div class="row">
             <div class="col-md-12">
