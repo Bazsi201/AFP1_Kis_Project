aa8304f12655f2e078e24df9215a8bad3429f64b
Bazsi201 <m.bazsi201@gmail.com>
2022-10-09 12:00:28 +0200

    sikeres kérés esetén vizsgálat, hogy minden mező kitöltésre került-e

--- Project/Program/code.php
+++ Project/Program/code.php
@@ -3,7 +3,9 @@
 
     if (isset($_POST['update_student']))
     {
-        
+        if (empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['course'])) {
+
+        }
     }
 
     if (isset($_POST['save_student']))
