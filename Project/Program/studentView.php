506d9d404816922fc3f3af25b5c3acf941bbb2b1
szlzs05 <zsoltisw@gmail.com>
2022-10-09 13:01:43 +0200

    táblázat kialakítása bootstrap osztályok alkalmazásával

--- Project/Program/studentView.php
+++ Project/Program/studentView.php
@@ -1,3 +1,18 @@
 <?php include('includes/header.php'); ?>
 
+<div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+
 <?php include('includes/footer.php'); ?>
