920c98df8c3a7ecbd63ce032aa03ec9d9d13a36d
kmartin001 <94145536+kmartin001@users.noreply.github.com>
2022-10-11 11:38:38 +0200

    Update emailExists.php
    Elírás korrekció

--- Project/Program/includes/emailExists.php
+++ Project/Program/includes/emailExists.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-$_SESSION['message'] = "A student with this email is already exists in the database.";
+$_SESSION['message'] = "A student with this email already exists in the database.";
 header("Location: index.php");
 exit(0);
 
